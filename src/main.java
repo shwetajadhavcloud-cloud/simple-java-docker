74083fb3f9397c893843a8514272b53da9a86ccb
Shweta Jadhav <shwetajadhav.cloud@gmail.com>
2025-11-05 10:42:36 +0530

    Rename class 'Main' to 'main'
    Renamed class 'Main' to 'main' to follow naming conventions.

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, Docker! Current date: " + currentDate);
